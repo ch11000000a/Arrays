f5d7b1fca10623a013a01dfa5a8d6a75d0671bb4
ch11000000a <1100000011010111@mail.ru>
2022-10-16 20:45:59 +0700

    encoding

--- shift/main.cpp
+++ shift/main.cpp
@@ -1,16 +1,16 @@
-#include<iostream>
+п»ї#include<iostream>
 using namespace std;
 void main() {
 	setlocale(LC_ALL, "");
 	const int n = 10;
 	int arr[n] = { 1,2,3,4,5,6,7,8,9,10 };
-	//Вывод исходного массива
+	//Р’С‹РІРѕРґ РёСЃС…РѕРґРЅРѕРіРѕ РјР°СЃСЃРёРІР°
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
 	}
 	cout << endl;
-	// Сдвиг массива
+	// РЎРґРІРёРі РјР°СЃСЃРёРІР°
 
 	for (int i = 0; i < n; i++)
 	{
@@ -24,7 +24,7 @@ void main() {
 	}
 	cout << endl;
 
-	//Вывод сдвинутого массива
+	//Р’С‹РІРѕРґ СЃРґРІРёРЅСѓС‚РѕРіРѕ РјР°СЃСЃРёРІР°
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
